5742ac4a0956a4ca627fae2540b189d795f66f91
mcramileux <kristineramilo21@gmail.com>
2023-06-02 12:50:27 +1000

    add PORT

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const express = require('express');
 const app = express();
 
 // initializing the PORT for Heroku deployment
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 // calling server to the route files
 const api = require('./routes/api.js');
